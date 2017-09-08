777bcc5b336c12b33eaedde0a4b766799b844707
Saheel Wagh <saheelwagh@gmail.com>
2017-09-08 08:06:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string==='I love You Grandma'){
-    return 'I Love You Too';
+    return 'I love you, too';
   }
   else if(string === string.toLowerCase()){
-    return "I can't hear you";
+    return "I can't hear you!";
   }
   else if(string === string.toUpperCase()){
     return "YES INDEED!"
